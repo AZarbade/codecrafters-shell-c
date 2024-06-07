be20ad10a26a219bfd67113ab673ef2a725550b0
AZarbade <adwaitzarbade@gmail.com>
2024-06-07 10:53:09 +0530

    pass stage

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   printf("$ ");
@@ -7,5 +8,12 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  // sanitize input
+  input[strlen(input - 1)] = '\0';
+
+  // Command not found!
+  printf("%s: command not found\n", input);
+
   return 0;
 }
