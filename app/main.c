d3465e77bbc548fa8dede6045eec0c2c740684ca
AZarbade <adwaitzarbade@gmail.com>
2024-06-07 10:59:03 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,19 +1,19 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-  printf("$ ");
-  fflush(stdout);
+  bool run = true;
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-
-  // sanitize input
-  input[strlen(input) - 1] = '\0';
-
-  // Command not found!
-  printf("%s: command not found\n", input);
+  while (run) {
+    printf("$ ");
+    fflush(stdout);
+    char input[100];
+    if (fgets(input, 100, stdin) != NULL) {
+      input[strlen(input) - 1] = '\0';
+      printf("%s: command not found\n", input);
+    }
+  }
 
   return 0;
 }
