d5dd726d6b5f0b3d5cdbc174ab08f51ed8460ef9
AZarbade <adwaitzarbade@gmail.com>
2024-06-07 11:07:30 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -3,15 +3,23 @@
 #include <string.h>
 
 int main() {
-  bool run = true;
+  bool exit_bool = false;
 
-  while (run) {
+  while (!exit_bool) {
     printf("$ ");
     fflush(stdout);
     char input[100];
+
     if (fgets(input, 100, stdin) != NULL) {
+      // input sanitization
       input[strlen(input) - 1] = '\0';
-      printf("%s: command not found\n", input);
+
+      // exit condition check
+      if (strcmp(input, "exit 0") == 0) {
+        exit_bool = true;
+      } else {
+        printf("%s: command not found\n", input);
+      }
     }
   }
 
