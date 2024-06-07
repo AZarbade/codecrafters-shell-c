ae763b09ee1c07125e314f649f843848802d18d0
AZarbade <adwaitzarbade@gmail.com>
2024-06-07 17:17:41 +0530

    Stage #IP1: runs executable

--- app/main.c
+++ app/main.c
@@ -32,6 +32,49 @@ char *parse_env() {
   }
 }
 
+void find_exec(char *inner_command, char *tokens[], int tok_count) {
+  bool found = false;
+  char buf[1024];
+  for (int i = 0; i < tok_count; i++) {
+    int len = snprintf(buf, sizeof(buf), "%s/%s", tokens[i], inner_command);
+    if (len >= 0 && access(buf, F_OK) == 0) {
+      printf("%s is %s\n", inner_command, buf);
+      found = true;
+      break;
+
+    } else {
+      continue;
+    }
+  }
+
+  if (!found) {
+    printf("%s: command not found\n", inner_command);
+  }
+}
+
+void run_exec(char *inner_command, char *tokens[], int tok_count) {
+  bool found = false;
+  char buf[1024];
+  for (int i = 0; i < tok_count; i++) {
+    int len = snprintf(buf, sizeof(buf), "%s/%s", tokens[i], inner_command);
+    if (len >= 0 && access(buf, F_OK) == 0) {
+      found = true;
+
+      char *args[] = {NULL};
+      execv(buf, args);
+
+      break;
+
+    } else {
+      continue;
+    }
+  }
+
+  if (!found) {
+    printf("%s: command not found\n", inner_command);
+  }
+}
+
 int main() {
   char *path = parse_env();
 
@@ -50,6 +93,8 @@ int main() {
     printf("%s\n", tokens[i]);
   }
 
+  free(pathCopy);
+
   // main loop
   bool exit_bool = false;
 
@@ -66,12 +111,16 @@ int main() {
       if ((match_command(input, "exit 0")) == 0) {
         // exit condition check
         exit_bool = true;
+
       } else if ((match_command(input, "echo")) == 0) {
         // command: echo
+        // TODO: get rid of hardcoded command count
+        //           v
         for (int i = 5; i < strlen(input); i++) {
           printf("%c", input[i]);
         }
         printf("\n");
+
       } else if ((match_command(input, "type") == 0)) {
         // extract inner_command
         char inner_command[100];
@@ -80,28 +129,14 @@ int main() {
           strcat(inner_command, temp);
         }
 
-        bool found = false;
-        char buf[1024];
-        for (int i = 0; i < tok_count; i++) {
-          int len =
-              snprintf(buf, sizeof(buf), "%s/%s", tokens[i], inner_command);
-          if (len >= 0 && access(buf, F_OK) == 0) {
-            printf("%s is %s\n", inner_command, buf);
-            found = true;
-            break;
-          } else {
-            continue;
-          }
-        }
-
-        if (!found) {
-          printf("%s: command not found\n", inner_command);
-        }
+        // find executable
+        find_exec(inner_command, tokens, tok_count);
 
         // set inner_command to null
         strcpy(inner_command, "");
 
       } else {
+        run_exec(input, tokens, tok_count);
         // command not found
         printf("%s: command not found\n", input);
       }
